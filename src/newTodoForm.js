fc523d6db8c100bda16665525217e10dd64bd5f2
samroodAli <devsamrood@gmail.com>
2021-07-15 19:39:20 +0530

    Form:Give name attributes to the form input elements

--- src/newTodoForm.js
+++ src/newTodoForm.js
@@ -10,15 +10,16 @@ import {
 
 const newTodoForm = () => {
   return form([
-    textInput("Enter title"),
+    textInput("Enter title", "", { name: "title" }),
     textArea("", "", {
       placeholder: "Enter description for the todo",
       required: "",
+      name: "description",
     }),
     labelFor("Enter due date", "date"),
-    dateInput("Enter due date", "", { id: "date" }),
+    dateInput("Enter due date", "", { id: "date", name: "date" }),
     labelFor("Enter priority", "todo-priority"),
-    numberInput("", "", { min: 0, max: 10, id: "priority" }),
+    numberInput("", "", { min: 0, max: 10, id: "priority", name: "priority" }),
     submitButton(),
   ]);
 };
