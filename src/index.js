2be7c2681ddb6fb42bfb9b4070ce771475cf216e
samroodAli <devsamrood@gmail.com>
2021-07-15 19:55:15 +0530

    Todo:Destructure new todo on form submission event

--- src/index.js
+++ src/index.js
@@ -1,12 +1,17 @@
-import Form from "./newTodoForm";
+import newTodoForm from "./newTodoForm.js";
+import ToDo from "./todo.js";
+import todoCard from "./todoCard";
+
 const content = document.getElementById("content");
 
-const form = Form();
+const form = newTodoForm();
 
 function submit(form, event) {
   event.preventDefault();
-  const toDoInfo = Array.from(form.elements).map((ele) => ele.value);
-  console.log(toDoInfo);
+  const [title, description, date, priority] = Array.from(form.elements).map(
+    (ele) => ele.value
+  );
+  console.log(title, description, date, priority);
 }
 
 form.addEventListener("submit", (event) => submit(form, event));
