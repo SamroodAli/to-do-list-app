dd1b85eb658fe6c7834f904c8c807c8451a79a3e
samroodAli <devsamrood@gmail.com>
2021-07-15 19:46:28 +0530

    Form:Get data from the form on submission

--- src/index.js
+++ src/index.js
@@ -1,4 +1,14 @@
 import Form from "./newTodoForm";
 const content = document.getElementById("content");
 
-content.appendChild(Form());
+const form = Form();
+
+function submit(form, event) {
+  event.preventDefault();
+  const toDoInfo = Array.from(form.elements).map((ele) => ele.value);
+  console.log(toDoInfo);
+}
+
+form.addEventListener("submit", (event) => submit(form, event));
+
+content.appendChild(form);
