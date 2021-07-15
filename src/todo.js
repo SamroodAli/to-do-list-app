d1c543d6f1edde8a00cdf844e832adbbd38268b2
samroodAli <devsamrood@gmail.com>
2021-07-15 16:20:25 +0530

    Todo:Set properties in constructor for to dos

--- src/todo.js
+++ src/todo.js
@@ -1,2 +1,9 @@
-class ToDo {}
+class ToDo {
+  constructor(title, description, date, priority) {
+    this.title = title;
+    this.description = description;
+    this.date = date;
+    this.priority = priority;
+  }
+}
 export default ToDo;
