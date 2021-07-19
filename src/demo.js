021d804f4ed06d4427648366ce70a631da06338d
samroodAli <devsamrood@gmail.com>
2021-07-19 14:50:45 +0530

    Fix:Use renderer on id to render on content

---
+++ src/demo.js
@@ -0,0 +1,38 @@
+import { newTodoForm, updateTodoForm } from "./newTodoForm.js";
+import Category from "./category.js";
+import newCategoryForm from "./newCategoryForm.js";
+import { div, h2, section } from "./tags.js";
+import "./style.scss";
+
+// const content = document.getElementById("content");
+
+// const project = new Category("default");
+// const categories = [project];
+
+// const categoryForm = newCategoryForm();
+
+// function newCategory(form, event) {
+//   event.preventDefault();
+//   const [name] = Array.from(form.elements).map((ele) => ele.value);
+//   const newCategory = new Category(name);
+//   categories.push(newCategory);
+//   updateTodoForm(categories);
+// }
+
+// categoryForm.addEventListener("submit", (event) =>
+//   newCategory(categoryForm, event)
+// );
+
+// function render() {
+//   content.append(
+//     section(
+//       [
+//         div([h2("New To-Do Form"), newTodoForm(categories)]),
+//         div([h2("New Category Form"), categoryForm]),
+//       ],
+//       "form-section"
+//     ),
+//     div("", "", { id: "todos" })
+//   );
+// }
+// // render();
--- src/index.js
+++ src/index.js
@@ -1,38 +1,5 @@
-import { newTodoForm, updateTodoForm } from "./newTodoForm.js";
-import Category from "./category.js";
-import newCategoryForm from "./newCategoryForm.js";
-import { div, h2, section } from "./tags.js";
-import "./style.scss";
+import { renderOnId } from "./render.js";
+import { h1 } from "./tags.js";
 
-const content = document.getElementById("content");
-
-const project = new Category("default");
-const categories = [project];
-
-const categoryForm = newCategoryForm();
-
-function newCategory(form, event) {
-  event.preventDefault();
-  const [name] = Array.from(form.elements).map((ele) => ele.value);
-  const newCategory = new Category(name);
-  categories.push(newCategory);
-  updateTodoForm(categories);
-}
-
-categoryForm.addEventListener("submit", (event) =>
-  newCategory(categoryForm, event)
-);
-
-function render() {
-  content.append(
-    section(
-      [
-        div([h2("New To-Do Form"), newTodoForm(categories)]),
-        div([h2("New Category Form"), categoryForm]),
-      ],
-      "form-section"
-    ),
-    div("", "", { id: "todos" })
-  );
-}
-// render();
+const render = renderOnId("content");
+render(h1("Hello world"));
