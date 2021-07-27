2107aeca18017d3fc1af2326a4f3f37e6c2a52f9
samroodAli <devsamrood@gmail.com>
2021-07-27 22:26:54 +0530

    Test:Write function to test renderOn higher order function

--- __tests__/render.test.js
+++ __tests__/render.test.js
@@ -1,10 +1,23 @@
 import "@testing-library/jest-dom";
 import document from "./document.js";
-import { render } from "../src/api/render.js";
+import { render, renderOn } from "../src/api/render.js";
 
 test("test render function to append child with parent", () => {
   const div = document.createElement("div");
   const p = document.createElement("p");
   expect(render(div, p)).toBe(div);
+  expect(div.innerHTML).toBe(p.outerHTML);
+});
+
+describe("testing higher order function renderON", () => {
+  const div = document.createElement("div");
+  const p = document.createElement("p");
+  const actual = renderOn(div);
+  actual(p);
+
+  it("should return a function", () => {
+    expect(typeof renderOn(div)).toBe("function");
+  });
+
   expect(div.innerHTML).toBe(p.outerHTML);
 });
