2b6eb1c2ee301846acb1cbcd8cb6ce14663e3df9
samroodAli <devsamrood@gmail.com>
2021-07-27 21:13:21 +0530

    Test:Test all the heading tags from the tags api

--- __tests__/tags.test.js
+++ __tests__/tags.test.js
@@ -74,3 +74,57 @@ test("testing footer wrapper for dom element", () => {
   expect(actual.innerText).toEqual("Hello world");
   expect(actual.outerHTML).toEqual('<footer class="className"></footer>');
 });
+
+test("testing h1 wrapper for dom element", () => {
+  const actual = h1("Hello world", "className");
+  const expected = document.createElement("div");
+  expected.className = "className";
+  expect(actual).toHaveClass("className");
+  expect(actual.innerText).toEqual("Hello world");
+  expect(actual.outerHTML).toEqual('<h1 class="className"></h1>');
+});
+
+test("testing h2 wrapper for dom element", () => {
+  const actual = h2("Hello world", "className");
+  const expected = document.createElement("div");
+  expected.className = "className";
+  expect(actual).toHaveClass("className");
+  expect(actual.innerText).toEqual("Hello world");
+  expect(actual.outerHTML).toEqual('<h2 class="className"></h2>');
+});
+
+test("testing h3 wrapper for dom element", () => {
+  const actual = h3("Hello world", "className");
+  const expected = document.createElement("div");
+  expected.className = "className";
+  expect(actual).toHaveClass("className");
+  expect(actual.innerText).toEqual("Hello world");
+  expect(actual.outerHTML).toEqual('<h3 class="className"></h3>');
+});
+
+test("testing h4 wrapper for dom element", () => {
+  const actual = h4("Hello world", "className");
+  const expected = document.createElement("div");
+  expected.className = "className";
+  expect(actual).toHaveClass("className");
+  expect(actual.innerText).toEqual("Hello world");
+  expect(actual.outerHTML).toEqual('<h4 class="className"></h4>');
+});
+
+test("testing h5 wrapper for dom element", () => {
+  const actual = h5("Hello world", "className");
+  const expected = document.createElement("div");
+  expected.className = "className";
+  expect(actual).toHaveClass("className");
+  expect(actual.innerText).toEqual("Hello world");
+  expect(actual.outerHTML).toEqual('<h5 class="className"></h5>');
+});
+
+test("testing h6 wrapper for dom element", () => {
+  const actual = h6("Hello world", "className");
+  const expected = document.createElement("div");
+  expected.className = "className";
+  expect(actual).toHaveClass("className");
+  expect(actual.innerText).toEqual("Hello world");
+  expect(actual.outerHTML).toEqual('<h6 class="className"></h6>');
+});
