02df6aed61575576441b1ea6508034720f17f183
samroodAli <devsamrood@gmail.com>
2021-07-27 21:09:25 +0530

    Test:Import tags for testing

--- __tests__/tags.test.js
+++ __tests__/tags.test.js
@@ -1,8 +1,33 @@
 import "@testing-library/jest-dom";
-import { div } from "../src/api/tags.js";
+import {
+  div,
+  section,
+  header,
+  main,
+  footer,
+  h1,
+  h2,
+  h3,
+  h4,
+  h5,
+  h6,
+  p,
+  span,
+  ul,
+  li,
+  nav,
+  a,
+  button,
+  img,
+} from "../src/api/tags.js";
 
-test("testing div function in tags", () => {
-  const actual = div("Hello world");
+test("testing div wrapper for dom element", () => {
+  const actual = div("Hello world", "className");
+  const expected = document.createElement("div");
+  expected.className = "className";
+  expect(actual).toHaveClass("className");
+  expect(actual.innerText).toEqual("Hello world");
+  expect(actual.outerHTML).toEqual('<div class="className"></div>');
 });
 
 test("testing div wrapper for dom element", () => {
