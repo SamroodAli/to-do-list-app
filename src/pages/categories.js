4eb224602ec843276ef24eb6aaaa613f17583f94
samroodAli <devsamrood@gmail.com>
2021-07-29 20:03:44 +0530

    Fix:Fix categorySection to have break lines

--- src/pages/categories.js
+++ src/pages/categories.js
@@ -2,6 +2,9 @@ import { categoryValues } from '../api/storage.js';
 import { section } from '../api/tags.js';
 import categoryPartial from '../views/category.js';
 
-const categorySection = (categories = categoryValues()) => section(categories.map((category) => categoryPartial(category)));
+const categorySection = (categories = categoryValues()) => {
+  const categoryPartials = categories.map((category) => categoryPartial(category));
+  return section(categoryPartials);
+};
 
 export default categorySection;
