6d3b55d70a26dd5ad277353d2e9c8ff6d629a01a
samroodAli <devsamrood@gmail.com>
2021-07-29 18:59:23 +0530

    Test:TodoCard for each todo markup test

---
+++ __tests__/views/todoCard.test.js
@@ -0,0 +1,14 @@
+import todoCard from "../../src/views/todoCard.js";
+
+test("test Todo Card from views", () => {
+  const todo = {
+    title: "New task",
+    description: "testing project",
+    date: "24/12/2020",
+    priority: 10,
+  };
+  const actual = todoCard(todo);
+  const expected =
+    '<div class="card mx-auto w-100 m-1" id="todo-undefined"><p></p><p></p><p></p><p></p><p></p><button></button><button></button></div>';
+  expect(actual.outerHTML).toEqual(expected);
+});
