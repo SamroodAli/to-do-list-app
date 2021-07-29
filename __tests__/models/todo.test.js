f64e2aa8b4bc61de7ae2092b4ef5ad5ffeb6fc5f
samroodAli <devsamrood@gmail.com>
2021-07-29 12:42:48 +0530

    Test:Test Todo model class from models with toEqual

---
+++ __tests__/models/todo.test.js
@@ -0,0 +1,12 @@
+import Todo from "../../src/models/todo.js";
+
+test("Todo model class", () => {
+  const actual = new Todo("New task", "testing project", "24/12/2020", 10);
+  const expected = {
+    title: "New task",
+    description: "testing project",
+    date: "24/12/2020",
+    priority: 10,
+  };
+  expect(actual).toEqual(expected);
+});
