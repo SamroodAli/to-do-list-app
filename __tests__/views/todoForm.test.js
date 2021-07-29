f6f0888c2ddb29f6762f5769f1688a8f28042700
samroodAli <devsamrood@gmail.com>
2021-07-29 19:14:35 +0530

    Test:Options creator from todoForm in views

--- __tests__/views/todoForm.test.js
+++ __tests__/views/todoForm.test.js
@@ -13,3 +13,10 @@ test("test new Todo Form", () => {
     '<form><input type="text" placeholder="Enter title" required="" name="title" value=""><textarea placeholder="Enter description for the todo" required="" name="description" value="undefined"></textarea><label for="date"></label><input type="date" placeholder="Enter due date" required="" id="date" name="date" value="undefined"><label for="todo-priority"></label><input type="number" placeholder="" required="" min="0" max="10" id="priority" name="priority" value="undefined"><label for="categoriesSelect"></label><select id="categoriesSelect"><option value="undefined" selected="selected"></option></select><input type="submit" placeholder="undefined" required=""></form>';
   expect(actual.outerHTML).toEqual(expected);
 });
+
+test("Options creator in views", () => {
+  const actual = optionsCreator(categories, categories[0]);
+  expect(typeof actual).toBe("object");
+  expect(Array.isArray(actual)).toBe(true);
+  expect(actual[0].outerHTML).toEqual('<option value="undefined"></option>');
+});
