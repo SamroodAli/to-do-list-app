b35e31ebc4b1cab451d0fa26b790458b3da91dae
samroodAli <devsamrood@gmail.com>
2021-07-29 18:41:43 +0530

    Test:New category form from views

---
+++ __tests__/views/categoryForm.test.js
@@ -0,0 +1,8 @@
+import categoryForm from "../../src/views/categoryForm.js";
+
+test("new CategoryForm from views", () => {
+  const actual = categoryForm();
+  const expected =
+    '<form><input type="text" placeholder="Enter category name" required="" name="name"><input type="submit" placeholder="undefined" required=""></form>';
+  expect(actual.outerHTML).toEqual(expected);
+});
