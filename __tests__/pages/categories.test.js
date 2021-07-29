4011eab7c177ea1160da0f9d8f03ab229760d786
samroodAli <devsamrood@gmail.com>
2021-07-29 16:32:28 +0530

    Test:Test categories page from pages

--- __tests__/pages/categories.test.js
+++ __tests__/pages/categories.test.js
@@ -3,5 +3,8 @@ import document from "../document.js";
 import CategorySection from "../../src/pages/categories.js";
 
 test("categories section markup", () => {
-  const actual = CategorySection;
+  const actual = CategorySection();
+  expect(actual.outerHTML).toEqual(
+    '<section><div class="card"><h3></h3><button class="btn-secondary"></button></div></section>'
+  );
 });
